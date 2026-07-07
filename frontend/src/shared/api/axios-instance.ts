449b59c97ebbb24766d79cdc88c18fdcb98193f5
MrFrozenNik <nikolayrutin@gmail.com>
2026-07-08 01:52:12 +0500

    add .env.example containing api url, add axios instance

---
+++ frontend/src/shared/api/axios-instance.ts
@@ -0,0 +1,6 @@
+import axios, {type AxiosInstance} from "axios";
+
+export const apiClient: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+})
