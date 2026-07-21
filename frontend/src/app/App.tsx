e7fe8fa27f25e64bd65338404223f6fb9dcbe5aa
MrFrozenNik <nikolayrutin@gmail.com>
2026-07-21 22:46:12 +0500

    add slices, implement react router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,19 +1,23 @@
-import { LoginForm } from "@/features/login";
+import {Route, Routes} from "react-router-dom";
+import {LoginForm} from "@/features/login";
 import {RegisterForm} from "@/features/register";
-import { useUser } from "@/entities/user";
+import {useUser} from "@/entities/user";
 
 function App() {
-    const { user, isLoading, isAuthenticated } = useUser();
+    const {user, isLoading, isAuthenticated} = useUser();
 
     if (isLoading) return <p>Загрузка...</p>;
 
     return (
-        <>
-            логин:
-            {isAuthenticated ? <p>Привет, {user?.name}!</p> : <LoginForm />}
-            регистер:
-            {isAuthenticated ? <p>Привет, {user?.name}!</p> : <RegisterForm />}
-        </>
+        <Routes>
+            <Route path="/" element={<>
+                логин:
+                {isAuthenticated ? <p>Привет, {user?.name}!</p> : <LoginForm/>}
+                регистер:
+                {isAuthenticated ? <p>Привет, {user?.name}!</p> : <RegisterForm/>}
+            </>}/>
+        </Routes>
+
     );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import {BrowserRouter} from "react-router-dom";
 import App from '@/app/App.tsx'
 import {UserProvider} from "@/entities/user";
 import '@/shared/styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <UserProvider>
-        <App/>
-      </UserProvider>
+        <BrowserRouter>
+            <UserProvider>
+                <App/>
+            </UserProvider>
+        </BrowserRouter>
     </StrictMode>,
 )
