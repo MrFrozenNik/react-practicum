12e8a8d9da6ab5d2e024ca336cdbd3b9f595b696
MrFrozenNik <nikolayrutin@gmail.com>
2026-07-21 22:59:20 +0500

    remove unused files, add header files

--- frontend/src/shared/ui/Text/Text.tsx
+++ frontend/src/shared/ui/Text/Text.tsx
@@ -1,6 +1,5 @@
 import type {ComponentPropsWithoutRef, ElementType} from 'react';
 import clsx from 'clsx';
-import styles from './Text.module.scss';
 
 type TextSize = 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | '7xl' | '8xl' | '9xl';
 type TextWeight = 'regular' | 'medium' | 'semibold' | 'bold';
@@ -25,8 +24,8 @@ export const Text = <T extends ElementType = 'p'>({
     return (
         <Tag
             className={clsx(
-                styles[`text-${size}`],
-                styles[`font-${weight}`],
+                `text-${size}`,
+                `font-${weight}`,
                 className,
             )}
             {...props}
